244aab47f75a85d992be339d5cc568ad6f54825e
howard50b <howard50b@gmail.com>
2017-04-25 22:43:34 -0400

    add early stopping

--- assignment4/tagging/bilstm_crf.py
+++ assignment4/tagging/bilstm_crf.py
@@ -4,7 +4,7 @@ import numpy as np
 import logging
 from rnn_bucket_loader import Loader
 
-FILENAME = 'bilstm_crf_sample'
+FILENAME = 'test'
 logging.basicConfig(filename='./log/{}.log'.format(FILENAME), level=logging.DEBUG)
 
 
@@ -29,66 +29,98 @@ class RNN:
                                                               sequence_length=self.seq_len,
                                                               inputs=self.embedded_x)
             self.rnn_output = tf.concat(2, outputs, name='rnn_output')
-            self.rnn_outputs_flat = tf.reshape(self.rnn_output, shape=[-1, 2 * cell_size]) # (N * sentence_len, 2 * cell_size)
+
+            # (N * sentence_len, 2 * cell_size)
+            self.rnn_outputs_flat = tf.reshape(self.rnn_output, shape=[-1, 2 * cell_size])
 
         with tf.name_scope('output-layer'):
-            W = tf.Variable(tf.truncated_normal([2 * cell_size, num_classes], stddev=1.0 / np.sqrt(num_classes)), name='W')
+            W = tf.Variable(tf.truncated_normal([2 * cell_size, num_classes], 
+                                                stddev=1.0 / np.sqrt(num_classes)), name='W')
             b = tf.Variable(tf.zeros([num_classes]), name='b')
-
-            self.logits_flat = tf.matmul(self.rnn_outputs_flat, W) + b # (N * sentence_len, num_classes)
+            
+            # (N * sentence_len, num_classes)
+            self.logits_flat = tf.matmul(self.rnn_outputs_flat, W) + b
 
             self.logits = tf.reshape(self.logits_flat, [-1, sentence_len, num_classes])
-            log_likelihood, self.trans_params = tf.contrib.crf.crf_log_likelihood(self.logits, self.labels, self.seq_len)
+            log_likelihood, self.trans_params = tf.contrib.crf.crf_log_likelihood(self.logits, 
+                                                                                  self.labels, 
+                                                                                  self.seq_len)
 
         with tf.name_scope('loss'):
             self.loss = tf.reduce_mean(-log_likelihood)
         
-    def train(self, train_x, train_y, num_epoch=3, batch_size=64):
-        batch_pairs = self._extract_batch(train_x, train_y, num_epoch, batch_size, shuffle=True)
-
+    def train(self, train_x, train_y, num_epoch=3, batch_size=64, tune_ratio=None):
+        # set optimizer
         optimizer = tf.train.AdamOptimizer(0.01)
+
+        # clipping gradient
         tvars = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), 1)
         train_op = optimizer.apply_gradients(zip(grads, tvars))
 
         self.sess.run(tf.global_variables_initializer())
 
+        # prepare batches to train on
+        batch_pairs = self._extract_batch(train_x, train_y, num_epoch, batch_size, tune_ratio, shuffle=True)
         batches_per_epoch = int((len(train_x) - 1) / batch_size) + 1
         step = 0
-        for batch_x, batch_y in batch_pairs:
+        tune_info = []
+
+        # start training 
+        for batch in batch_pairs:
+            batch_x, batch_y, tune_x, tune_y = batch
             _, loss = self.sess.run([train_op, self.loss], 
                                     feed_dict={self.inputs: batch_x, self.labels: batch_y})
-            acc = self.calculate_accuracy(batch_x, batch_y)
             if step % 10 == 0:
-                print('Epoch: {}, Step: {}, loss: {:.6f}, accuracy: {:.2f}'.format(
-                      int(step / batches_per_epoch), step, loss, acc))
-                logging.debug('Epoch: {}, Step: {}, loss: {:.6f}, accuracy: {:.2f}'.format(
-                              int(step / batches_per_epoch), step, loss, acc))
+                batch_acc = self.calculate_accuracy(batch_x, batch_y)
+                print('[Batch]: Epoch: {}, Step: {}, loss: {:.6f}, accuracy: {:.4f}'.format(
+                      int(step / batches_per_epoch), step, loss, batch_acc))
+                logging.debug('[Batch]: Epoch: {}, Step: {}, loss: {:.6f}, accuracy: {:.4f}'.format(
+                              int(step / batches_per_epoch), step, loss, batch_acc))
 
-                print('Prediction for the first row in batch:')
-                print(self.predict(batch_x[:1, :]))
-                print('Label for the first row in batch:')
-                print(self.sess.run(self.labels, feed_dict={self.labels: batch_y[:1, :]}))
+            if step % 100 == 0 and tune_x is not None and tune_y is not None:
+                tune_loss = self.sess.run(self.loss, feed_dict={self.inputs: tune_x, self.labels: tune_y})
+                tune_acc = self.calculate_accuracy(tune_x, tune_y)
+                print('[Tune]: Epoch: {}, Step: {}, loss: {:.6f}, accuracy: {:.4f}'.format(
+                      int(step / batches_per_epoch), step, tune_loss, tune_acc))
+                logging.debug('[Tune]: Epoch: {}, Step: {}, loss: {:.6f}, accuracy: {:.4f}'.format(
+                              int(step / batches_per_epoch), step, tune_loss, tune_acc))
+                tune_info.append(tune_loss)
             step += 1
 
-    def _extract_batch(self, train_x, train_y, num_epoch, batch_size, shuffle=True):
+    def _extract_batch(self, train_x, train_y, num_epoch, batch_size, tune_ratio, shuffle=True):
         data = np.hstack((train_x, train_y))
-        data_size = data.shape[0]
-        batches_per_epoch = int((data_size - 1) / batch_size) + 1
+        if tune_ratio:
+            tune_size = int(data.shape[0] * tune_ratio)
+        else:
+            tune_size = 0
+        train_size = data.shape[0] - tune_size
+        batches_per_epoch = int((train_size - 1) / batch_size) + 1
 
         for epoch in range(num_epoch):
-            if shuffle:
-                shuffle_idxs = np.random.permutation(np.arange(data_size))
-                shuffled_data = data[shuffle_idxs]
+            # extract tuning set
+            tune_idxs = np.random.choice(len(data), tune_size, replace=False)
+            if tune_ratio:
+                tune_x = data[tune_idxs][:, :train_x.shape[1]]
+                tune_y = data[tune_idxs][:, train_x.shape[1]:]
             else:
-                shuffled_data = data
+                tune_x = None
+                tune_y = None
+
+            # shuffle and build batches with the reset
+            data_ = np.delete(data, tune_idxs, axis=0)
+            if shuffle:
+                shuffle_idxs = np.random.permutation(np.arange(train_size))
+                shuffled_data = data_[shuffle_idxs]
+            else:
+                shuffled_data = data_
 
             for batch_num in range(batches_per_epoch):
                 start_idx = batch_num * batch_size
-                end_idx = min((batch_num + 1) * batch_size, data_size)
+                end_idx = min((batch_num + 1) * batch_size, train_size)
                 batch_x = shuffled_data[start_idx:end_idx][:, :train_x.shape[1]]
                 batch_y = shuffled_data[start_idx:end_idx][:, train_x.shape[1]:]
-                yield batch_x, batch_y
+                yield batch_x, batch_y, tune_x, tune_y
 
     def predict(self, x):
         logits, seq_len, trans_params = self.sess.run([self.logits, self.seq_len, self.trans_params],
@@ -142,7 +174,7 @@ if __name__ == '__main__':
               embed_size=128, 
               cell_size=128,
               num_layers=1)
-    rnn.train(train_x, train_y, num_epoch=3, batch_size=64)
+    rnn.train(train_x, train_y, num_epoch=2, batch_size=64, tune_ratio=None)
     dev_accuracy = rnn.calculate_accuracy(dev_x, dev_y)
     print('Dev accuracy:', dev_accuracy)
     logging.debug('Dev accuracy: {}'.format(dev_accuracy))
